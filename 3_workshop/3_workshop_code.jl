ea3e0061d740da90000984c7159b6e7ddb750923
Owen Miller <owenchristianmiller@gmail.com>
2020-08-31 15:10:17 -0400

    workshop 3 - add tolerance, add double precision

--- 3_workshop/3_workshop_code.jl
+++ 3_workshop/3_workshop_code.jl
@@ -9,17 +9,20 @@ Owen Miller
 ====#
 
 # Question 1
-
-function secant_method(f, x0, x1, iter)
+FACC = .00001
+function secant_method(f, x0::Float64, x1::Float64, iter)
     x2 = 0
     for i in 0:iter
         x2 = x1 - f(x1) * (x1 - x0) / (float(f(x1) - f(x0)))
         x0, x1 = x1, x2
+        if abs(f(x2)) < FACC
+            break
+        end
     end
     return x2
 end
 
-function false_postition(f, x0, x1, iter)
+function false_postition(f, x0::Float64, x1::Float64, iter)
     if f(x0) * f(x1) >= 0
         println("Bad x0 & x1: ", x0, " & ", x1)
         return -1
@@ -34,16 +37,12 @@ function false_postition(f, x0, x1, iter)
         else
             x0 = x2
         end
+        if abs(f(x2)) < FACC
+            break
+        end
     end
     return x2
 end
 
-function f(x)
-    return x^2 - 612
-end
 
-root_sec = secant_method(f, 10, 30, 5)
-root_fal = false_postition(f, 10, 30, 5)
-
-println("Secant Method: ", root_sec)
-println("False Position: ", root_fal)
+# Question 2
