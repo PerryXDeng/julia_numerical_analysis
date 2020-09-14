39a7c04a1aa7c7ef8efd4942e13b6a9f0679fb4e
PerryXDeng <pxd256@protonmail.com>
2020-09-14 15:38:12 -0400

    plain fpi

--- 6_workshop/6_workshop_code.jl
+++ 6_workshop/6_workshop_code.jl
@@ -20,11 +20,17 @@ function steffensen(g, x0::Float64;
             x0=x1
             break
         end
+        if 3i+1 == max_iter
+            break
+        end
 
         x2 = g(x1)
         if abs(x2 - x0) < x_convergence_threshold
             println("CONVERGENCE at i=", 3i + 2)
             x0=x2
+            break
+        end
+        if 3i+2 == max_iter
             break
         end
 
@@ -34,13 +40,32 @@ function steffensen(g, x0::Float64;
             x0=x3
             break
         end
+        if 3i+3 == max_iter
+            break
+        end
+
         x0 = x3
     end
     println("x=",x0)
 end
 
+function plain_fpi(g, x0::Float64;
+                   max_iter::Int64=50,
+                   x_convergence_threshold::Float64=1e-8)
+    for i in 1:max_iter
+        x1 = g(x0)
+        if abs(x1 - x0) < x_convergence_threshold
+            println("CONVERGENCE at i=", i)
+            x0=x1
+            break
+        end
+        x0=x1
+    end
+    println("x=",x0)
+end
 # Question 2
 
 # Question 3
+
 println("g(x)=cos(x); x0 = 1; expected root approx 0.7")
 #steffensen(x::Float64->cos(x), )
